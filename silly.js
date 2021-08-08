89e6c39b6037dd1e9c3c2738517e4096525bbeae
A-ELG <angellopezgracia@gmail.com>
2021-08-08 00:56:00 -0500

    pic adjusted

---
+++ silly.js
@@ -0,0 +1,74 @@
+/*
+█████████████████████████████████████:os/.
+█████████████████████████████`-:+syhdho:ohhhdhyo/-.
+█████████████████████████./ohmmmmmmmmho--:shdmmmmmmdy+-`
+█████████████████████`-ohmmmmmmddddmmdh:---ohhddddmmmmmms-`
+███████████████████-odmmmmmmdddddmmmmdh+---+yshddmdmdmmmmmdo:`
+████████████████`/hmmmmdddddmddmmmmmmmhy--/y/-+yhddddmddmmmmmmy/.
+██████████████.odmmmddddddmddmmmmmdddmdh+os:---:yhhdmdddddddddmmmh+.
+████████████.ommmmddddddddmdmNmmddddmmdhy:------hoyhddddddddddhyshmmh+-`
+██████████`+dNmmdddddddddmmmmmddddddddmdh+-----+y-:ydddddddhhsoooooshmmdo:.██████████████████```
+█████████.yNmmmddmddddddmmNmmmmmddddddddhs----+y//yhdddddhysoooooooshmmmNmdy+-`█████████`.:+ooos/`
+████████:dNmdddddmmddddmmNmddddddddddddmdh/-/ss+shddddddhsoooooooshmmmmmmmmmmmdy+:.`█`-+sso+::-oho
+███████+mmmdmddmmmmdddmmmmmddddddddddddmdhyoo/ohddddddhsooooooooymmmdmNmddddmmmmmmmhsss+/-------yh:
+██████+mmmddddmmNmdmddmmmdmdddddddddddddmhh/:shddddddhooooooooodmmddmNmdmdddmdddmddhs/----------ohs
+█████+mmmddddmNNmddmdmNmddddddddddddddddmdhyhdddddddyoooooooosmmmddmNmdmdddddmdmdhs/------------:hh.
+████:mmmdddmmNNmmdddmNmmddddddddddddddmdmdhddddddddyoooooooosmmmmdmmmmdddddddmmdh+---------------yh/
+███.dNmddmmmmhNmdddmNmddddddddddddddddddddddddddddyoooooooosmmddddmNmddddddddmmy/----------------sh+
+██`yNmddmdmm:dmddddmNmdddddddddddddddddddddddddddyoooooooosmmdddmmNmddddddddmNd:-----------------sh+
+██/Nmddddmm:+NmddmdmmmmmmmmmmmmmmmmmmdddddddddddhooooooooodNmddmdmNmddddddddmNs----------------:-sh+
+█.dmddddmm/█hNmmmmmNNNNmmmmmmNNmmmmmmmmmddddddddsoooooooosNmdddddmNdmdddddddmNo---------------o:-hh:
+█yNmmmmmmy██yymhso++yNNho/shdmNmdyooosyhdmmmmmmdsoooooooodNmdddddNmdddddddmdmN+--------------o+-/hh`
+:mmdys+/-`███`md/----:smMmy+smNMMMmo-----::/oydmmmdysoooommdddddmNmdddddddmdmNo------------:o/--sh+
+--.███████████ymNds:--oms.:yhdNMMMMMd-----------/oymmmdysmmddddddNmdddddddddmNs-------o:-:oo---+hy`
+██████████████/h+ohmmmm:██████`+NMMMMd--------------:+ymNNNmmddddmNdddddddddmNd:---:smh-oo:---:hh-
+██████████████`ys---dm-█████████/MMMMMy-----------------/shmNNmmdmNmddddddddmNNNoshNNh++:----:yh/
+████████████/.█-y/-sN:██████████-MMMMMM:-------------------:dNmNNmNmdmdddddddmNNMds+/:------:yh/
+████████████-hdhmdyNs██████████`yMMMMMMs-------------------sMy`:odNNmmmddmdddmNNMs------:+/:yy:
+██████████████.:hdso.█████████`yMMMMMMMd------------------+Md`███`hNNNNmmmmdmdmNNmo++oyddo+hs.
+███████████████`ys-.████`███.+mMMMMMMMMm------------------mN-████+NMMMMNNNNmmddmNNmdhyo/:shy`
+███████████████.ho-`███.dyymNMMNMMMMMMMN:----------------sMs███.yMMMMMMMNNNdmNmmmNms++/+hdmm.
+███████████████:h/-`████ydmMMh-`-mMMMMMN:---------------:mN--/hNMMMMMMMMNNN.`:oymNNNNNNNNNNm.
+███████████████:h+-`████.sodM:██:NMMMMMN----------------oMMNNMNysmMMMMMNmmy████`-/sdNmmmmmNy`
+███████████████-ho-.█████---dNhdNMMMMMMm----------------hMMMMM:██+MMMMdyhh.████`--:dmdmmdmN+
+███████████████`ys--██████`odmNMMMMMMMMd---------------:mMMMMMy+sNMMm+/oo.█████.--/mmdddmNm.
+████████████████shoyo`█████-shddmmmmmdNh-----:::::-----/NMMMMMMMMMNmy:..`█████`---sNmdddmNo
+████████████████+my+yo`█████`.:+oosoo/ms--/ossssssso:--/NmNNNNNNmmddho.██████`----dNmddmNd`
+████████████████.yhho/o/██████████████d/-//::-----:+o:-+doyhdddhhys/.`██████.yy/-oNmddmNN/
+█████████████████+h+.sh:..``█████████`+----------------:+█`.---..`███████``so-://mNmdmmNs
+█████████████████`sy:+:------.......-----+o:------:/:----``███████████``-h+:y/--hNmddmNm.
+██████████████████.yy:------------::------:/-----:++:------..........----/y.---yNmddmNN+
+███████████████████-yy:-----------:s/---------------------//-----------------:hNmmmmNNh`
+████████████████████-yy:-----------/s+------------------/so/----------------+dNmmmmNNm-
+█████████████████████.oy/-----------:os+/::----------:/oo/----:/---------:odNmmmmmmmNs
+███████████████████████/ys:------------/+osoooo+oooooo/:------dmho+++osydmNmmmmmmmmNN-
+████████████████████████`+yo:--------------:::///::----------sNmmmmmmmmmmmmmmmmdddmNd`
+██████████████████████████.+yo/-----------------------------+NNmmmmmmmmmmmmmmmmdmmNNs
+████████████████████████████`/ss+:-------------------------:dNmmmmmmmmmmmmmmNmdmmmmN+
+███████████████████████████████./sso/:---------------------oNmddddddmmmmmmNNmmmNmymN/
+██████████████████████████████████`-/syso++//:-------------hNmmmmmmmmmdyydNmmmNdsodN/
+█████████████████████████████████████:h:::/:::-------------:/+ossoo+//odNmmmNmdsosmN/
+
+
+
+
+
+
+// attempt #2
+
+Nil = n => c => n;
+Cons = x => xs => n => c => c(x)(xs);
+
+// oh neat it actually does work without lazy evaluation
+fromArray = (xs, i=0) => n => c => i < xs.length ? c(xs[i])(fromArray(xs, i+1)) : n;
+toArray = (xs, r=[]) => xs(r)(a => as => toArray(as, [r.push(a), r][1]));
+
+concat = xs => ys => xs(ys)(a => as => n => c => c(a)(concat(as)(ys)));
+reverse = (xs, ys=(n => c => n)) => n => c => xs(r)(a => as => reverse(as, c(a)(ys)));
+
+zip = xs => ys => xs(xs)(a => as => ys(ys)(b => bs => n => c => c([a, b])(zip(as)(bs))));
+map = f => xs => xs(xs)(a => as => n => c => c(f(a))(map(f)(as)));
+
+head = xs => xs(undefined)(a => as => a);
+tail = xs => xs(undefined)(a => as => as);
+*/
